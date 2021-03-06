c816c8780aeba3d049c4b1e517fa8811b013fbca
Silvio Dayube <scarige@gmail.com>
2021-03-06 17:16:54 -0300

    Implements persistent AB testing

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../header/Header';
 import play from '../../common/vectors/play-button.svg';
 import heroImage from '../../common/images/image-header.png';
 import './Hero.scss';
 
 function Hero() {
+  const [ABTestResult, setABTest] = useState(parseInt(localStorage.getItem('ABTest'), 10));
+
+  useEffect(() => {
+    if (Number.isNaN(ABTestResult)) {
+      const ABValue = Math.floor(Math.random() * 2);
+      localStorage.setItem('ABTest', ABValue);
+      setABTest(parseInt(localStorage.getItem('ABTest'), 10));
+    }
+  });
+
   return (
     <section id="hero-section">
       <Header />
       <div id="hero-wrapper">
         <article>
-          <h1>Easily create or join a local nanny share with Hapu</h1>
+          <h1>
+            { ABTestResult
+              ? 'Easily create or join a local nanny share with Hapu'
+              : 'Create the childcare you need at a price you can afford'}
+          </h1>
           <p>
-            Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new
-            flexible, affordable solutions in childcare.
+            { ABTestResult
+              ? 'Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.'
+              : 'Connect with other local families to share a nanny from as low as $10.00/hr each. Create your family profile today to get started.'}
           </p>
           <div>
             <img src={play} alt="Play button" />
