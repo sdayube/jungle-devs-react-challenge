7a1f780d65a026dfbb5afcbf7ffbdb38c8e379d0
Silvio Dayube <scarige@gmail.com>
2021-03-06 01:27:42 -0300

    Adds Sass and styles header

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import badge from '../../common/vectors/badge.svg';
-import './Header.scss';
 
 function Header() {
   return (
     <header>
-      <div id="left-header-wrapper">
+      <div>
         <img src={badge} alt="logo" />
         <nav>
-          <ul id="nav-list">
+          <ul>
             <li><a href="#">Create Your Nanny Share</a></li>
             <li><a href="#">Browse Shares</a></li>
             <li><a href="#">Our Story</a></li>
           </ul>
         </nav>
       </div>
-      <div id="right-header-wrapper">
-        <button id="share-button" type="button">Become a Nanny Share Host</button>
-        <button id="sign-in-button" type="button">Sign In</button>
+      <div>
+        <button type="button">Become a Nanny Share Host</button>
+        <button type="button">Sign In</button>
       </div>
     </header>
   );
